a6ed4ec8207bbc71463c7da98bf95c106c6c85d0
Irving Montes <imontes@MacBook-Pro-de-Irving.local>
2018-12-03 18:10:31 -0600

    sw modified

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,8 @@ var  swLocation = '/sw.js';
 if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register( swLocation );
 }
+if (navigator.onLine) {
+    alert("Tienes conexion a internet :)");
+}else{
+    alert("No tienes conexion a internet :(");
+}
